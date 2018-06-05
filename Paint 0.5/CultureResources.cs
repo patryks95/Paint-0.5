6bd8d46f1ad876d2e917c0724f5045dd565c94a8
Patryk <patryk-smol@wp.pl>
2018-06-05 03:10:25 +0200

    Dodaj pliki projektów.

---
+++ Paint 0.5/CultureResources.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+namespace Paint_0._5
+{
+    public class CultureResources
+    {
+        private static ObjectDataProvider resourceProvider =
+          (ObjectDataProvider)App.Current.FindResource("Resources");
+
+        public Properties.Resources GetResourceInstance()
+        {
+            return new Properties.Resources();
+        }
+
+        public static void ChangeCulture(CultureInfo culture)
+        {
+            Properties.Resources.Culture = culture;
+            resourceProvider.Refresh();
+        }
+    }
+}
---
+++ Paint 0.5/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Xceed.Wpf.Toolkit;
+
+namespace Paint_0._5
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            CultureResources.ChangeCulture(Properties.Settings.Default.DefaultCulture);
+
+
+            InitializeComponent();
+            UpdateStatusLabel();
+
+        }
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            bool setPolishCulture = (sender == PolishItem);
+
+            CultureResources.ChangeCulture(new CultureInfo(setPolishCulture ? "pl" : "en"));
+            PolishItem.IsChecked = setPolishCulture;
+            EnglishItem.IsChecked = !setPolishCulture;
+            UpdateStatusLabel();
+
+        }
+        private void UpdateStatusLabel()
+        {
+            DateTime dt = DateTime.Now;
+            StatusBarData.Text = dt.ToString("d", Properties.Resources.Culture) + "    " +
+                dt.ToString("t", Properties.Resources.Culture);
+        }
+
+        private void ButtonNewColor_Click(object sender, RoutedEventArgs e)
+        {
+
+            Window okno = new Window();
+            ColorCanvas temp = new ColorCanvas();
+            
+
+        }
+    }
+}
